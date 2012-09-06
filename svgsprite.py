13ffbab77ba6534c456eac620f774dfbd91d5c32
Tom Najdek <tom@doppnet.com>
2012-09-06 18:28:00 +0100

    Modified to use inkscape to determine width and position of elements. Positioning and viewbox resizing is now possible.

--- svgsprite.py
+++ svgsprite.py
@@ -13,6 +13,25 @@ sys.path.append(scour_path)
 from scour import scourString, _options_parser, serializeTransform
 from svg_transform import svg_transform_parser
 
+
+def which(program):
+
+	def is_exe(fpath):
+		return os.path.isfile(fpath) and os.access(fpath, os.X_OK)
+
+	fpath, fname = os.path.split(program)
+	if fpath:
+		if is_exe(program):
+			return program
+	else:
+		for path in os.environ["PATH"].split(os.pathsep):
+			exe_file = os.path.join(path, program)
+			if is_exe(exe_file):
+				return exe_file
+
+	return None
+
+
 def readin_xml(filename):
 	try:
 		xml = parse(args.filename)
@@ -25,7 +44,7 @@ def readin_xml(filename):
 	return xml
 
 if __name__ == '__main__':
-	parser = argparse.ArgumentParser(description='Convert SVG file to ninja SVG sprite')
+	parser = argparse.ArgumentParser(description='Convert SVG file to ninja-style SVG sprite. Default behaviour is to find all elements with matching ids, move them to 0,0, resize the document viewbox to the width & height of the largest identified items, add a css-based trick to allow targeting sprites with hash in the url and finally use scour to shave off some bytes from the svg prior to outpiting it to stdout.')
 	parser.add_argument('filename', help="SCG file to process")
 	parser.add_argument('--id-prefix', default=None, help="If specified only ids with this prefix will be processed")
 	parser.add_argument('--ids', default=None, help="Coma-separated list. If specified only these ids will be processed")
@@ -34,7 +53,7 @@ if __name__ == '__main__':
 	parser.add_argument('--dont-strip-comments', action='store_true', default=False, help="If specified comments won't be stripped. Has no effect when using --skip-scour")
 	parser.add_argument('--dont-remove-metadata', action='store_true', default=False, help="If specified metadata from the document won't be stripped. Has no effect when using --skip-scour")
 	parser.add_argument('--disable-viewboxing', action='store_true', default=False, help="If specified sizing/viewboxing won't be performed. Has no effect when using --skip-scour")
-
+	parser.add_argument('--disable-viebox-resizing', action='store_true', default=False, help="If specified viewbox resizing won't be performed. Depending on the input svg file might not work in some cases when used in conjuction with '--disable-viewboxing' or '--skip-scour'. In most cases makes no sense when used in conjuction with '--skip-positioning'")
 
 	args = parser.parse_args()
 	if(args.ids):
@@ -43,33 +62,50 @@ if __name__ == '__main__':
 		print "Please specify either id-prefix or ids but not both."
 		sys.exit()
 
+	inkscape = None
 	if(not args.skip_positioning):
+		inkscape = which('inkscape')
+		if(inkscape == None):
+			print "Could not find inkscape in system path. Either install inkscape (http://inkscape.org/) or disable positioning with --skip-positioning"
+			sys.exit()
 		xml = readin_xml(args.filename)
 
-	groups = xml.getElementsByTagName('g')
-	for g in groups:
+	items = []
+	for svgtag in ["a", "altGlyph", "altGlyphDef", "altGlyphItem", "animate", "animateColor", "animateMotion", "path", "animateTransform", "circle", "clipPath", "color-profile", "cursor", "defs", "desc", "ellipse", "feBlend", "filter-primitive-reference", "g", "image", "line", "linearGradient", "marker", "mask", "path", "pattern", "polygon", "polyline", "radialGradient", "rect", "stop", "text", "tref", "tspan", "text", "use"]:
+		items = items + xml.getElementsByTagName(svgtag)
+
+	maxwidth = 0
+	maxheight = 0
+	for g in items:
 		gid = g.getAttribute('id')
 		if(args.ids and gid in args.ids
 			or args.id_prefix and gid.startswith(args.id_prefix)
 			or not (args.ids or args.id_prefix)):
 			g.setAttribute('class', 'sprite')
 			if(not args.skip_positioning):
+
+				# this will be massively slow
 				offsetx = float(subprocess.check_output(['inkscape', '-z', '-X', '--query-id=%s' % gid, args.filename]))
 				offsety = float(subprocess.check_output(['inkscape', '-z', '-Y', '--query-id=%s' % gid, args.filename]))
-				if(offsety > 0 or offsetx > 0):
+				width = float(subprocess.check_output(['inkscape', '-z', '-W', '--query-id=%s' % gid, args.filename]))
+				height = float(subprocess.check_output(['inkscape', '-z', '-H', '--query-id=%s' % gid, args.filename]))
+
+				if(width > maxwidth):
+					maxwidth = width
+				if(height > maxheight):
+					maxheight = height
+
+				if(offsety == 0 and offsetx == 0):
 					g.setAttribute('transform', "translate(-%f,-%f)" % (offsetx, offsety))
 				if(offsety > 0 or offsetx > 0):
 					val = g.getAttribute('transform')
 					translated = False
 					if(val != ''):
-						print gid
 						transform = svg_transform_parser.parse(val)
 						for t in transform:
 							if(t[0] == 'translate'):
-								import ipdb; ipdb.set_trace()
-								t[1][0] = (float(t[1][0]) + offsetx) * -1
-								t[1][1] = (float(t[1][1]) + offsety) * -1
-								print t
+								t[1][0] = (offsetx - float(t[1][0])) * -1
+								t[1][1] = (offsety - float(t[1][1])) * -1
 								translated = True
 					if(translated):
 						new_transform = serializeTransform(transform)
@@ -94,12 +130,16 @@ if __name__ == '__main__':
 	elif(not xml):
 		xml = readin_xml(args.filename)
 
-	svg = xml.getElementsByTagName('svg')
+	svg = xml.getElementsByTagName('svg')[0]
+	if(not args.disable_viebox_resizing):
+		viewbox = '0 0 %f %f' % (maxwidth, maxheight)
+		svg.setAttribute('viewBox', viewbox)
+
 	defs = xml.createElement('defs')
 	style = xml.createElement('style')
 	styleText = xml.createTextNode("svg .sprite { display: none }\nsvg .sprite:target { display: inline } ")
 	style.appendChild(styleText)
 	defs.appendChild(style)
-	svg[0].appendChild(defs)
+	svg.appendChild(defs)
 
-	# print >>sys.stdout, xml.toxml()
+	print >>sys.stdout, xml.toxml()
